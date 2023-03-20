5e015525ca9d66ebd5efc321730abc44069a47ac
cdv74997 <100449958+cdv74997@users.noreply.github.com>
2023-03-19 18:12:08 -0700

    Delete base directory

--- base/templatetags/musicmeet_custom_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def get_item(dictionary, key):
-    # I had to make this custom tag because the core Django developers refuse to make in-template dictionary access a built-in feature. Grrrr.
-    return dictionary.get(key)
